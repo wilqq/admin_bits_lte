f845267b2251d58b974318cb984d186ab14a9123
Mateusz Wilczyński <majko7@interia.pl>
2015-02-04 09:38:19 +0100

    Handle sorting classes

--- spec/dummy/app/assets/javascripts/admin_bits.js
+++ spec/dummy/app/assets/javascripts/admin_bits.js
@@ -4,3 +4,13 @@
 //= require admin-lte
 //= require dataTables/jquery.dataTables
 //= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
+
+
+$(function(){
+  $('a.sort_asc').each(function() {
+     $(this).parent().addClass('sorting_asc');
+  });
+  $('a.sort_desc').each(function() {
+     $(this).parent().addClass('sorting_desc');
+  });
+});
