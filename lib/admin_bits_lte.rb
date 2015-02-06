56e268c94ee8505734dcf771b31b8ddb5837587a
Mateusz Wilczyński <majko7@interia.pl>
2015-02-06 12:21:55 +0100

    Call generator method

--- lib/admin_bits_lte.rb
+++ lib/admin_bits_lte.rb
@@ -2,5 +2,7 @@ require "admin_bits_lte/version"
 require "admin_bits_lte/generators/admin_bits_lte_generator"
 
 module AdminBitsLte
-  # Your code goes here...
+  def call_generator(params)
+    Rails::Generators.invoke('admin_bits_lte', [params['resource'], "--namespace=#{params['namespace']}"])
+  end
 end
