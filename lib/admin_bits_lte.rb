2e41309e0b9c0eb3c9392c0dcc70f684bc194443
Mateusz Wilczyński <majko7@interia.pl>
2015-02-06 12:37:29 +0100

    Class methods

--- lib/admin_bits_lte.rb
+++ lib/admin_bits_lte.rb
@@ -2,7 +2,13 @@ require "admin_bits_lte/version"
 require "admin_bits_lte/generators/admin_bits_lte_generator"
 
 module AdminBitsLte
-  def self.call_generator(params)
-    Rails::Generators.invoke('admin_bits_lte', [params['resource'], "--namespace=#{params['namespace']}"])
+  def self.included(base)
+    base.extend(ClassMethods)
+  end
+
+  module ClassMethods
+    def call_generator(params)
+      Rails::Generators.invoke('admin_bits_lte', [params['resource'], "--namespace=#{params['namespace']}"])
+    end
   end
 end
