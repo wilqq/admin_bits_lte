f02e50c89b839202fedefc45c6cc27da0e39dfe4
Mateusz Wilczyński <majko7@interia.pl>
2015-02-07 17:48:45 +0100

    Add views

--- Gemfile
+++ Gemfile
@@ -3,12 +3,13 @@ source 'https://rubygems.org'
 gem "rails", ">= 4.0.0"
 gem "sqlite3"
 
-gem 'admin_bits', git: 'https://github.com/wilqq/admin_bits'
+gem 'admin_bits', path: '/home/mateusz/projekty/admin_bits'
 gem 'bootstrap-sass'
 gem 'sass-rails', '~> 4.0.3'
 gem 'adminlte-rails'
 gem 'jquery-rails'
 gem 'jquery-datatables-rails'
+gem 'simple_form'
 
 gem 'pry'
 
--- lib/admin_bits_lte/generators/admin_bits_lte_generator.rb
+++ lib/admin_bits_lte/generators/admin_bits_lte_generator.rb
@@ -2,6 +2,8 @@ require 'rails/generators'
 require 'rbconfig'
 
 class AdminBitsLteGenerator < Rails::Generators::Base
+  include AdminBits::GeneratorHelpers
+
   argument :resource,
     :type => :string,
     :required => true,
@@ -14,45 +16,23 @@ class AdminBitsLteGenerator < Rails::Generators::Base
 
   self.source_paths << File.join(File.dirname(__FILE__), 'templates')
 
-  def create_layout
-    template 'layout.html.erb', 'app/views/layouts/admin_bits/layout.html.erb'
-    template 'stylesheets.css.scss', 'app/assets/stylesheets/admin_bits.css.scss'
-    template 'javascripts.js', 'app/assets/javascripts/admin_bits.js'
-  end
+  # def create_layout
+  #   template 'layout.html.erb', 'app/views/layouts/admin_bits/layout.html.erb'
+  #   template 'stylesheets.css.scss', 'app/assets/stylesheets/admin_bits.css.scss'
+  #   template 'javascripts.js', 'app/assets/javascripts/admin_bits.js'
+  # end
 
   def create_index
     template 'index.html.erb', "app/views/#{namespace}/#{resource}/index.html.erb"
     template 'filters.html.erb', "app/views/#{namespace}/#{resource}/_filters.html.erb"
   end
 
-  def add_assets_initializer
-    if (['admin_bits.css', 'admin_bits.js'] -  Rails.application.config.assets.precompile).any?
-      content = 'Rails.application.config.assets.precompile += %w(admin_bits.css admin_bits.js)'
-      path = 'config/initializers/assets.rb'
+  # def add_assets_initializer
+  #   if (['admin_bits.css', 'admin_bits.js'] -  Rails.application.config.assets.precompile).any?
+  #     content = 'Rails.application.config.assets.precompile += %w(admin_bits.css admin_bits.js)'
+  #     path = 'config/initializers/assets.rb'
 
-      File.write(path, content, mode: 'a')
-    end
-  end
-
-  protected
-
-  def namespace
-    options[:namespace]
-  end
-
-  def resource_name
-    (resource.singularize + '_resource')
-  end
-
-  def attribute_names
-    raw_resource = eval(resource.singularize.camelcase)
-    if raw_resource.class == Class
-      names = raw_resource.attribute_names
-      names.delete_if do |attribute|
-        attribute[-3..-1] == '_id'
-      end
-    else
-      []
-    end
-  end
+  #     File.write(path, content, mode: 'a')
+  #   end
+  # end
 end
